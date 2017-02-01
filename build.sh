62f07c20244312299c10dff66c8f9a3f8e71d406
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-02-01 09:28:28 -0800

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir download
 cd download
-wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.bz2
-tar --bzip2 -xf boost_1_63_0.tar.bz2
-du
+wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip
+printf "android:\n" > cdep-manifest.yml
+printf "  - file: boost_1_63_0.zip\n" >> cdep-manifest.yml
