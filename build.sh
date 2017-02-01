4176166a1e1d003240d856553b179c0fb22b46c3
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-02-01 09:50:02 -0800

    .

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip
-printf "coordinate:\n" >> cdep-manifest.yml
-printf "  groupId: com.github.jomof\n" >> cdep-manifest.yml
-printf "  artifactId: cmakeify\n" >> cdep-manifest.yml
-printf "  version: alpha-0.0.35\n" >> cdep-manifest.yml
-printf "android:\n" > cdep-manifest.yml
-printf "  - file: boost_1_63_0.zip\n" >> cdep-manifest.yml
-printf "    include: boost_1_63_0/boost\n" >> cdep-manifest.yml
+printf "coordinate:\r\n" >> cdep-manifest.yml
+printf "  groupId: com.github.jomof\r\n" >> cdep-manifest.yml
+printf "  artifactId: cmakeify\r\n" >> cdep-manifest.yml
+printf "  version: ${TRAVIS_TAG}\r\n" >> cdep-manifest.yml
+printf "android:\r\n" > cdep-manifest.yml
+printf "  - file: boost_1_63_0.zip\r\n" >> cdep-manifest.yml
+printf "    include: boost_1_63_0/boost\r\n\n" >> cdep-manifest.yml
+printf "linux:\r\n" > cdep-manifest.yml
+printf "  - file: boost_1_63_0.zip\r\n" >> cdep-manifest.yml
+printf "    include: boost_1_63_0/boost\r\n" >> cdep-manifest.yml
