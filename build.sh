b186d9551c668717d88a35716292d610878d239c
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-02-01 10:23:19 -0800

    .

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ printf "coordinate:\r\n" >> cdep-manifest.yml
 printf "  groupId: com.github.jomof\r\n" >> cdep-manifest.yml
 printf "  artifactId: cmakeify\r\n" >> cdep-manifest.yml
 printf "  version: ${TRAVIS_TAG}\r\n" >> cdep-manifest.yml
-printf "android:\r\n" > cdep-manifest.yml
+printf "android:\r\n" >> cdep-manifest.yml
 printf "  - file: boost_1_63_0.zip\r\n" >> cdep-manifest.yml
 printf "    include: boost_1_63_0/boost\r\n\n" >> cdep-manifest.yml
 printf "linux:\r\n" >> cdep-manifest.yml
