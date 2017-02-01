74bf3d5831f4c9d53eb150d2f4513d8ebc997026
Jomo Fisher <jomofisher@gmail.com>
2017-02-01 09:14:08 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.bz2
+wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.tar.bz
 tar --bzip2 -xf boost_1_63_0.tar.bz
