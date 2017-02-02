502fdc6f57a2e78f1212ec425a0abc74efc3f29d
Jomo Fisher <jomofisher@gmail.com>
2017-02-02 13:41:58 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 wget https://sourceforge.net/projects/boost/files/boost/1.63.0/boost_1_63_0.zip
 printf "coordinate:\r\n" >> cdep-manifest.yml
 printf "  groupId: com.github.jomof\r\n" >> cdep-manifest.yml
-printf "  artifactId: cdep-boost\r\n" >> cdep-manifest.yml
+printf "  artifactId: boost\r\n" >> cdep-manifest.yml
 printf "  version: ${TRAVIS_TAG}\r\n" >> cdep-manifest.yml
 printf "android:\r\n" >> cdep-manifest.yml
 printf "  - file: boost_1_63_0.zip\r\n" >> cdep-manifest.yml
