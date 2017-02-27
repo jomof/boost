638f7cf98cde38752dd4bf8dfcbb280574a2a0ce
Jomo Fisher <jomofisher@gmail.com>
2017-02-26 19:32:17 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,5 +18,17 @@ printf "    - file: boost_1_63_0.zip\r\n" >> cdep-manifest.yml
 printf "      sha256: $SHASUM256\r\n" >> cdep-manifest.yml
 printf "      size: $ARCHIVESIZE\r\n" >> cdep-manifest.yml
 printf "    include: boost_1_63_0\r\n\n" >> cdep-manifest.yml
+printf "example: |\r\n" >> cdep-manifest.yml
+printf "  #include <boost/numeric/ublas/matrix.hpp>\r\n" >> cdep-manifest.yml
+printf "  #include <boost/numeric/ublas/matrix.hpp>\r\n" >> cdep-manifest.yml
+printf "  void test() {\r\n" >> cdep-manifest.yml
+printf "     using namespace boost::numeric::ublas;\r\n"  >> cdep-manifest.yml
+printf "     matrix<double> m (3, 3);\r\n" >> cdep-manifest.yml
+printf "     for (unsigned i = 0; i < m.size1 (); ++ i) {\r\n" >> cdep-manifest.yml
+printf "       for (unsigned j = 0; j < m.size2 (); ++ j) {\r\n" >> cdep-manifest.yml
+printf "         m (i, j) = 3 * i + j;\r\n" >> cdep-manifest.yml
+printf "       }\r\n" >> cdep-manifest.yml
+printf "     }\r\n" >> cdep-manifest.yml
+printf "  }\r\n" >> cdep-manifest.yml
 
 cat cdep-manifest.yml
